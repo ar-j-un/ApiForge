d7fd3fbd699872e5107e64db53a96fe4d4bb818b
ar-j-un <arjun.menon@prophaze.com>
2026-07-29 12:43:27 +0530

    fix(user): rename relationship method from appication to applications
    Correct spelling of HasMany relationship method on User model.
    - Align relationship method with Laravel plural naming conventions

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,7 +31,7 @@ class User extends Authenticatable
         ];
     }
 
-    public function application(): HasMany
+    public function applications(): HasMany
     {
         return $this->hasMany(Application::class);
     }
