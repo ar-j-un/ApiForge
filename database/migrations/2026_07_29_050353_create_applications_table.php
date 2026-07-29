7f47de17d869684a009341412fee1e2c8db1ed7e
ar-j-un <arjun.menon@prophaze.com>
2026-07-29 12:57:19 +0530

    refactor(db): add foreign key constraint and cascade delete to user_id
    Update user_id foreign key definition on the applications table migration.
    - Attach constrained() and cascadeOnDelete() modifier to user_id column

--- database/migrations/2026_07_29_050353_create_applications_table.php
+++ database/migrations/2026_07_29_050353_create_applications_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('applications', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->ipAddress('address');
             $table->unsignedInteger('port');
