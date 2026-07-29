ace9cfa65810b8ac60bfabfa7f291e6b0ecbd34e
ar-j-un <arjun.menon@prophaze.com>
2026-07-29 16:28:47 +0530

    feat(views): include jQuery and DataTables CDN assets in app layout
    Add DataTables Bootstrap 5 styling and script dependencies to app.blade.php.
    
    - Include jQuery 3.7.1 runtime script via CDN
    - Add DataTables 2.1.8 CSS and JavaScript libraries with Bootstrap 5 integration
    - Provide global DataTable support for extending views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', config('app.name', 'ApiForge'))</title>
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.min.css">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
@@ -17,6 +18,9 @@
             @yield('content')
         </div>
     </div>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.datatables.net/2.1.8/js/dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.min.js"></script>
 @stack('scripts')
 </body>
 </html>
