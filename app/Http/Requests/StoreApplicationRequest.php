301526929559f55aec8b155591d75abc7cb681cb
ar-j-un <arjun.menon@prophaze.com>
2026-07-29 12:35:34 +0530

    feat(application): define validation rules in StoreApplicationRequest
    Add authorization and validation rules for application creation.
    
    - Authorize request execution
    - Add validation rules for name (required, max 255), address (valid IP), and port (integer 1-65535)

--- app/Http/Requests/StoreApplicationRequest.php
+++ app/Http/Requests/StoreApplicationRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Override;
 
 class StoreApplicationRequest extends FormRequest
 {
@@ -12,7 +13,7 @@ class StoreApplicationRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,7 +24,23 @@ class StoreApplicationRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required','string','max:255'],
+            'address' => ['required','ip'],
+            'port' => ['required','integer','between:1,65535'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Application name is required',
+            'name.string' => 'Name should be a string',
+            'name.max' => 'Maximum 255 characters',
+            'address.required' => 'Address is required',
+            'address.ip' => 'Should be an ip',
+            'port.required' => 'Port is required',
+            'port.integer' => 'Port should be an integer',
+            'port.between' => 'Port should be between 1 and 65535',
         ];
     }
 }
