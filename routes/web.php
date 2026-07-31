598570b2fcd67c48f601c46dd2d7d7952824f2ad
ar-j-un <arjun.menon@prophaze.com>
2026-07-31 17:44:54 +0530

    feat(routes): add edit and update routes for posts
    Register routes for editing and updating post entries.
    
    - Add GET route 'posts/{id}/edit' pointing to PostController@edit named 'posts.edit'
    - Add PUT and PATCH routes 'posts/{id}' pointing to PostController@update named 'posts.update'
    - Enforce authentication via existing auth middleware group

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,9 @@ Route::middleware('auth')->group(function () {
     Route::get('posts', [PostController::class, 'index'])->name('posts.index');
     Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
     Route::post('posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('posts/{id}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('posts/{id}', [PostController::class, 'update']);
+    Route::patch('posts/{id}', [PostController::class, 'update'])->name('posts.update');
 });
 
 
