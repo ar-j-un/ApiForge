d1a92d4c4c498a578a24752c6ba26b7d6e5bc09e
ar-j-un <arjun.menon@prophaze.com>
2026-08-03 10:57:46 +0530

    refactor(routes): use resource route for posts
    Simplify web routes by replacing individual post routes with Route::resource.
    
    - Replace manual GET, POST, PUT/PATCH, and DELETE definitions with Route::resource
    - Exclude 'show' route to match existing controller implementation

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,7 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('posts', [PostController::class, 'index'])->name('posts.index');
-    Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
-    Route::post('posts', [PostController::class, 'store'])->name('posts.store');
-    Route::get('posts/{id}/edit', [PostController::class, 'edit'])->name('posts.edit');
-    Route::put('posts/{id}', [PostController::class, 'update']);
-    Route::patch('posts/{id}', [PostController::class, 'update'])->name('posts.update');
-    Route::delete('posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::resource('posts', PostController::class)->except(['show']);
 });
 
 
