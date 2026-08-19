b10edecdc8174c73c0a7ead1f796c5b34985b6ae
ar-j-un <arjun.menon@prophaze.com>
2026-08-19 13:19:12 +0530

    feat(blog): add country input field to blog edit view
    Include country input field in the blog editing form.
    
    - Add country text input field with label to resources/views/blogs/edit.blade.php
    - Pre-populate field value using old('country', $blog->country) and set maximum length constraint to 500 characters

--- resources/views/blogs/edit.blade.php
+++ resources/views/blogs/edit.blade.php
@@ -37,6 +37,11 @@
       </div>
 
       <div class="mb-3">
+        <label for="country" class="form-label">Country</label>
+        <input type="text" name="country" id="country" class="form-control" value="{{ old('country', $blog->country) }}" maxlength="500">
+      </div>
+
+      <div class="mb-3">
         <label for="author_name" class="form-label">Author Name</label>
         <input type="text" name="author_name" id="author_name" class="form-control"
                value="{{ old('author_name', $blog->authorName) }}" required>
